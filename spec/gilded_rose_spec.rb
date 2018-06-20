d6334a2765fcf2c3f169173187de029d37f1c384
Mark Channer <markchanner@gmail.com>
2018-06-20 18:41:18 +0100

    Test does not add 1 to quality of 'Backstage passes..' with > 10 if quality == 50

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -112,6 +112,12 @@ describe GildedRose do
       expect(items[0].quality).to eq 3
     end
 
+    it "does not add 1 to quality of 'Backstage passes..' with > 10 if quality == 50" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 50
+    end
+
     it "adds 2 to quality if is 'Backstage passes..' with > 5 and <= 10 sell_in" do
       item0 = Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 2)
       item1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 2)

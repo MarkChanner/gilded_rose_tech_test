aef6d93859141fa1deb14eb7a3f2909f7a2f1dc4
Mark Channer <markchanner@gmail.com>
2018-06-20 16:43:18 +0100

    Test does not change quality of any item to below zero

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,6 +8,19 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it "does not change the quality of an item to below zero" do
+      item1 = Item.new("foo", 0, 0)
+      item2 = Item.new("Sulfuras, Hand of Ragnaros", 0, 0)
+      item3 = Item.new("Aged Brie", 0, 0)
+      item4 = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 0)
+      items = [item1, item2, item3, item4]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to be >= 0
+      expect(items[1].quality).to be >= 0
+      expect(items[2].quality).to be >= 0
+      expect(items[3].quality).to eq 0
+    end
   end
 
 end

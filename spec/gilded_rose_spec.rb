7de7ce9e0cc4f45b03189f8e1b9a442f775f8b1c
Mark Channer <markchanner@gmail.com>
2018-06-20 18:35:28 +0100

    Amend test 'changes the name' to 'does not change the name'

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,7 +3,7 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    it "changes the name" do
+    it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"

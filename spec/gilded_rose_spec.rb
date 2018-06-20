a1d4f7897357b8cf2cf3f008cf6adeac2a958c3d
Mark Channer <markchanner@gmail.com>
2018-06-20 18:17:43 +0100

    Amend item local variable names to start from zero, i.e item0, rather than item1

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -10,11 +10,11 @@ describe GildedRose do
     end
 
     it "subtracts 1 from sell_in for items, except 'Sulfuras, Hand of Ragnaros'" do
-      item1 = Item.new("foo", 0, 0)
-      item2 = Item.new("Sulfuras, Hand of Ragnaros", 0, 0)
-      item3 = Item.new("Aged Brie", 0, 0)
-      item4 = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 0)
-      items = [item1, item2, item3, item4]
+      item0 = Item.new("foo", 0, 0)
+      item1 = Item.new("Sulfuras, Hand of Ragnaros", 0, 0)
+      item2 = Item.new("Aged Brie", 0, 0)
+      item3 = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 0)
+      items = [item0, item1, item2, item3]
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq -1
       expect(items[1].sell_in).to eq 0
@@ -23,11 +23,11 @@ describe GildedRose do
     end
 
     it "does not change the quality of any item to below zero" do
-      item1 = Item.new("foo", 0, 0)
-      item2 = Item.new("Sulfuras, Hand of Ragnaros", 0, 0)
-      item3 = Item.new("Aged Brie", 0, 0)
-      item4 = Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 0)
-      items = [item1, item2, item3, item4]
+      item0 = Item.new("foo", 0, 0)
+      item1 = Item.new("Sulfuras, Hand of Ragnaros", 0, 0)
+      item2 = Item.new("Aged Brie", 0, 0)
+      item3 = Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 0)
+      items = [item0, item1, item2, item3]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 0
       expect(items[1].quality).to eq 0
@@ -35,11 +35,24 @@ describe GildedRose do
       expect(items[3].quality).to be >= 0
     end
 
+    it "never changes the quality of 'Sulfuras, Hand of Ragnaros'" do
+      item0 = Item.new("Sulfuras, Hand of Ragnaros", 11, 5)
+      item1 = Item.new("Sulfuras, Hand of Ragnaros", 4, 5)
+      item2 = Item.new("Sulfuras, Hand of Ragnaros", 0, 5)
+      item3 = Item.new("Sulfuras, Hand of Ragnaros", -4, 5)
+      items = [item0, item1, item2, item3]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 5
+      expect(items[1].quality).to eq 5
+      expect(items[2].quality).to eq 5
+      expect(items[3].quality).to eq 5
+    end
+
     it "subtracts 1 from quality for normal items (not Aged Brie, etc)" do
-      item1 = Item.new("foo", 8, 1)
-      item2 = Item.new("foo", 3, 1)
-      item3 = Item.new("foo", 0, 1)
-      items = [item1, item2, item3]
+      item0 = Item.new("foo", 8, 1)
+      item1 = Item.new("foo", 3, 1)
+      item2 = Item.new("foo", 0, 1)
+      items = [item0, item1, item2]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 0
       expect(items[0].quality).to eq 0
@@ -47,10 +60,10 @@ describe GildedRose do
     end
 
     it "does not subtract from quality for normal items if quality == 0" do
-      item1 = Item.new("foo", 8, 0)
-      item2 = Item.new("foo", 3, 0)
-      item3 = Item.new("foo", -1, 0)
-      items = [item1, item2, item3]
+      item0 = Item.new("foo", 8, 0)
+      item1 = Item.new("foo", 3, 0)
+      item2 = Item.new("foo", -1, 0)
+      items = [item0, item1, item2]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 0
       expect(items[0].quality).to eq 0
@@ -82,27 +95,27 @@ describe GildedRose do
     end
 
     it "adds 2 to quality if is 'Backstage passes..' with > 5 and <= 10 sell_in" do
-      item1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 2)
-      item2 = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 2)
-      items = [item1, item2]
+      item0 = Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 2)
+      item1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 2)
+      items = [item0, item1]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 4
       expect(items[1].quality).to eq 4
     end
 
     it "adds 3 to quality if is 'Backstage passes..' with > 0 and < 5 sell_in" do
-      item1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 2)
-      item2 = Item.new("Backstage passes to a TAFKAL80ETC concert", 4, 2)
-      items = [item1, item2]
+      item0 = Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 2)
+      item1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 4, 2)
+      items = [item0, item1]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 5
       expect(items[1].quality).to eq 5
     end
 
     it "sets quality to 0 if is 'Backstage passes..' with <= 0 sell_in" do
-      item1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 2)
-      item2 = Item.new("Backstage passes to a TAFKAL80ETC concert", -1, 2)
-      items = [item1, item2]
+      item0 = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 2)
+      item1 = Item.new("Backstage passes to a TAFKAL80ETC concert", -1, 2)
+      items = [item0, item1]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 0
       expect(items[1].quality).to eq 0

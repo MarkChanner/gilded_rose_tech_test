6374599363c08566b4c2503d500be2c45cc2944d
Mark Channer <markchanner@gmail.com>
2018-06-21 22:12:38 +0100

    Refactor to remove GildedRose dependency

--- spec/conjured_item_spec.rb
+++ spec/conjured_item_spec.rb
@@ -5,26 +5,27 @@ describe ConjuredItem do
 
   describe "#update" do
     it "degrades in Quality twice as fast as normal items" do
-      item0 = ConjuredItem.new("Conjured", 5, 4)
-      item1 = ConjuredItem.new("Conjured", 0, 4)
-      items = [item0, item1]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 2
-      expect(items[1].quality).to eq 0
+      item1 = ConjuredItem.new("Conjured", 5, 4)
+      item2 = ConjuredItem.new("Conjured", 0, 4)
+      item1.update
+      item2.update
+      expect(item1.quality).to eq 2
+      expect(item2.quality).to eq 0
     end
 
     it "does not change the qualty to below zero" do
-      item0 = ConjuredItem.new("Conjured", 5, 0)
-      item1 = ConjuredItem.new("Conjured", 5, 1)
-      item2 = ConjuredItem.new("Conjured", -1, 3)
-      item3 = ConjuredItem.new("Conjured", -1, 2)
-      items = [item0, item1, item2, item3]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 0
-      expect(items[1].quality).to eq 0
-      expect(items[2].quality).to eq 0
-      expect(items[3].quality).to eq 0
+      item1 = ConjuredItem.new("Conjured", 5, 0)
+      item2 = ConjuredItem.new("Conjured", 5, 1)
+      item3 = ConjuredItem.new("Conjured", -1, 3)
+      item4 = ConjuredItem.new("Conjured", -1, 2)
+      item1.update
+      item2.update
+      item3.update
+      item4.update
+      expect(item1.quality).to eq 0
+      expect(item2.quality).to eq 0
+      expect(item3.quality).to eq 0
+      expect(item4.quality).to eq 0
     end
-
   end
 end

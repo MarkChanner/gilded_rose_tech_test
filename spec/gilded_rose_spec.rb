e5bda0fd76c07510d72a4e6c078c843937f51a9f
Mark Channer <markchanner@gmail.com>
2018-06-21 22:34:38 +0100

    Test calls update method on each item in the passed-in array

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,17 +1,19 @@
 require 'gilded_rose'
 
 describe GildedRose do
+  let(:normal_item) { spy :normal_item, update: nil }
+  let(:backstage_pass_item) { spy :backstage_pass_item, update: nil }
+  let(:conjured_item) { spy :conjured_item, update: nil }
+  let(:sulfuras_item) { spy :sulfuras_item, update: nil }
 
-  it "does not change the quality of any item to below zero" do
-    item0 = NormalItem.new("foo", 0, 0)
-    item1 = SulfurasItem.new("Sulfuras, Hand of Ragnaros", 0, 0)
-    item2 = BrieItem.new("Aged Brie", 0, 0)
-    item3 = BackstagePassItem.new("Backstage passes to a TAFKAL80ETC concert", 1, 0)
-    items = [item0, item1, item2, item3]
-    GildedRose.new(items).update_quality()
-    expect(items[0].quality).to eq 0
-    expect(items[1].quality).to eq 0
-    expect(items[2].quality).to be >= 0
-    expect(items[3].quality).to be >= 0
+  describe "#update_quality" do
+    it "calls the update method once on each item in the passed-in array" do
+      items = [normal_item, backstage_pass_item, conjured_item, sulfuras_item]
+      GildedRose.new(items).update_quality()
+      expect(normal_item).to have_received(:update).once
+      expect(backstage_pass_item).to have_received(:update).once
+      expect(conjured_item).to have_received(:update).once
+      expect(sulfuras_item).to have_received(:update).once
+    end
   end
 end

2dbdb0ee26a124a65b842148771c6095e0251276
Mark Channer <markchanner@gmail.com>
2018-06-21 21:57:06 +0100

    Refactor to remove GildedRose dependency

--- spec/brie_item_spec.rb
+++ spec/brie_item_spec.rb
@@ -5,44 +5,43 @@ describe BrieItem do
 
   describe "#update" do
     it "adds just 1 to quality if sell_in value is > 0" do
-      items = [BrieItem.new("Aged Brie", 1, 2)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 3
+      item = BrieItem.new("Aged Brie", 1, 2)
+      item.update
+      expect(item.quality).to eq 3
     end
 
     it "does not increment quality if quality >= 50" do
-      items = [BrieItem.new("Aged Brie", 1, 50)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 50
+      item = BrieItem.new("Aged Brie", 1, 50)
+      item.update
+      expect(item.quality).to eq 50
     end
 
     it "adds 2 to quality if sell_in value is <= 0" do
-      items = [BrieItem.new("Aged Brie", 0, 2)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 4
+      item = BrieItem.new("Aged Brie", 0, 2)
+      item.update
+      expect(item.quality).to eq 4
     end
 
     it "only adds 1 to quality if quality == 49 (when sell_in value is <= 0)" do
-      items = [BrieItem.new("Aged Brie", 0, 49)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 50
+      item = BrieItem.new("Aged Brie", 0, 49)
+      item.update
+      expect(item.quality).to eq 50
     end
 
     it "subtracts 1 from sell_in" do
-      item0 = BrieItem.new("Aged Brie", 20, 0)
       item1 = BrieItem.new("Aged Brie", 10, 0)
       item2 = BrieItem.new("Aged Brie", 0, 0)
       item3 = BrieItem.new("Aged Brie", -10, 0)
-      items = [item0, item1, item2, item3]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq 19
-      expect(items[1].sell_in).to eq 9
-      expect(items[2].sell_in).to eq -1
-      expect(items[3].sell_in).to eq -11
+      item1.update
+      item2.update
+      item3.update
+      expect(item1.sell_in).to eq 9
+      expect(item2.sell_in).to eq -1
+      expect(item3.sell_in).to eq -11
     end
 
     it "does not change the quality to below zero" do
-      item = BackstagePassItem.new("Backstage passes to a TAFKAL80ETC concert", 1, 0)
+      item = BrieItem.new("Aged Brie", 1, 0)
       expect(item.quality).to eq 0
     end
   end

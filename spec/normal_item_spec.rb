09e7fff9375cdc118acd78a7f3acc6783d03e1b9
Mark Channer <markchanner@gmail.com>
2018-06-21 22:20:33 +0100

    Refactor to remove GildedRose dependency

--- spec/normal_item_spec.rb
+++ spec/normal_item_spec.rb
@@ -5,56 +5,53 @@ describe NormalItem do
 
   describe "#update" do
       it "does not change the name" do
-        items = [NormalItem.new("foo", 0, 0)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].name).to eq "foo"
+        item = NormalItem.new("foo", 0, 0)
+        expect(item.name).to eq "foo"
       end
 
       it "subtracts 1 from quality" do
-        item0 = NormalItem.new("foo", 8, 1)
-        item1 = NormalItem.new("foo", 3, 1)
+        item1 = NormalItem.new("foo", 8, 1)
         item2 = NormalItem.new("foo", 0, 1)
-        items = [item0, item1, item2]
-        GildedRose.new(items).update_quality()
-        expect(items[0].quality).to eq 0
-        expect(items[0].quality).to eq 0
-        expect(items[0].quality).to eq 0
+        item1.update
+        item2.update
+        expect(item1.quality).to eq 0
+        expect(item2.quality).to eq 0
       end
 
       it "does not decrement quality value if quality == 0" do
-        item0 = NormalItem.new("foo", 8, 0)
-        item1 = NormalItem.new("foo", 3, 0)
-        item2 = NormalItem.new("foo", -1, 0)
-        items = [item0, item1, item2]
-        GildedRose.new(items).update_quality()
-        expect(items[0].quality).to eq 0
-        expect(items[0].quality).to eq 0
-        expect(items[0].quality).to eq 0
+        item1 = NormalItem.new("foo", 8, 0)
+        item2 = NormalItem.new("foo", 3, 0)
+        item3 = NormalItem.new("foo", -1, 0)
+        item1.update
+        item2.update
+        item3.update
+        expect(item1.quality).to eq 0
+        expect(item2.quality).to eq 0
+        expect(item3.quality).to eq 0
       end
 
       it "subtracts 2 from quality when sell_in passed" do
-        items = [NormalItem.new("foo", -1, 2)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].quality).to eq 0
+        item = NormalItem.new("foo", -1, 2)
+        item.update
+        expect(item.quality).to eq 0
       end
 
       it "subtracts just 1, not 2, (when sell_in value is <= 0) from quality if quality == 1" do
-        items = [NormalItem.new("foo", -1, 1)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].quality).to eq 0
+        item = NormalItem.new("foo", -1, 1)
+        item.update
+        expect(item.quality).to eq 0
       end
 
       it "subtracts 1 from sell_in" do
-        item0 = NormalItem.new("foo", 20, 0)
         item1 = NormalItem.new("foo", 10, 0)
         item2 = NormalItem.new("foo", 0, 0)
         item3 = NormalItem.new("foo", -10, 0)
-        items = [item0, item1, item2, item3]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq 19
-        expect(items[1].sell_in).to eq 9
-        expect(items[2].sell_in).to eq -1
-        expect(items[3].sell_in).to eq -11
+        item1.update
+        item2.update
+        item3.update
+        expect(item1.sell_in).to eq 9
+        expect(item2.sell_in).to eq -1
+        expect(item3.sell_in).to eq -11
       end
   end
 end

3ce8582049f7379839327f8bbd4cc01367b171e7
Mark Channer <markchanner@gmail.com>
2018-06-21 17:51:40 +0100

    Test guilded rose does not change the qualty to below zero

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -199,9 +199,25 @@ describe GildedRose do
 
     describe "on Conjured items" do
       it "degrades in Quality twice as fast as normal items" do
-        items = [Item.new("Conjured", 5, 4)]
+        item0 = Item.new("Conjured", 5, 4)
+        item1 = Item.new("Conjured", 0, 4)
+        items = [item0, item1]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq 2
+        expect(items[1].quality).to eq 0
+      end
+
+      it "does not change the qualty to below zero" do
+        item0 = Item.new("Conjured", 5, 0)
+        item1 = Item.new("Conjured", 5, 1)
+        item2 = Item.new("Conjured", -1, 3)
+        item3 = Item.new("Conjured", -1, 2)
+        items = [item0, item1, item2, item3]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+        expect(items[1].quality).to eq 0
+        expect(items[2].quality).to eq 0
+        expect(items[3].quality).to eq 0
       end
     end
 
